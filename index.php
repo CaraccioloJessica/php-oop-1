b0aa67acb69cc4e03de4df1b59d97ff06d220338
Jessica <jesscara.993@gmail.com>
2023-01-24 15:37:03 +0100

    instances

--- index.php
+++ index.php
@@ -13,3 +13,9 @@ class Movie
     $this->duration = $duration;
   }
 }
+
+$movie1 = new Movie("Black Panther", "2018", "134 min");
+$movie2 = new Movie("Iron Man", "2008", "126 min");
+var_dump($movie1);
+echo "<br><br>";
+var_dump($movie2);
