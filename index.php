33a693f1ac6b086f58c29921a04f71a4bd7b9e7e
Jessica <jesscara.993@gmail.com>
2023-01-24 16:48:06 +0100

    added some html style to getHtml(), formatted code

--- index.php
+++ index.php
@@ -17,16 +17,27 @@ class Movie
   // metodo che racchiude gli attributi della classe, da richiamare quando andremo a stampare in pagina
   public function getHtml()
   {
-    return
-      $this->title
-      . ' - ' . $this->genre
-      . ' - ' . $this->year
-      . ' - ' . $this->duration;
+    return " Titolo: " .
+      $this->title .
+      "<br>" .
+      " Genere: " .
+      $this->genre .
+      "<br>" .
+      " Anno: " .
+      $this->year .
+      "<br>" .
+      " Durata: " .
+      $this->duration;
   }
 }
 
 // creato array con le diverse istanze
-$movies = [new Movie('Black Panther', 'Azione', '2018', '134 min'), new Movie('La città incantata', 'Animazione', '2001', '125 min')];
+$movies = [
+  new Movie("Black Panther", "Azione", "2018", "134 min"),
+  new Movie("La città incantata", "Animazione", "2001", "125 min"),
+  new Movie("Stardust", "Fantastico", "2007", "130 min"),
+];
+
 // $movie1 = new Movie('Black Panther', 'Azione', '2018', '134 min');
 // $movie2 = new Movie('La città incantata', 'Animazione', '2001', '125 min');
 // var_dump($movie1);
@@ -36,5 +47,5 @@ $movies = [new Movie('Black Panther', 'Azione', '2018', '134 min'), new Movie('L
 // cicla le istanze all'interno dell'array movies e le stampa su pagina
 foreach ($movies as $movie) {
   echo $movie->getHtml();
-  echo '<br>';
+  echo "<br><br>";
 }
