459eeeee92f4559cac7c56ccc0d1431f6ba7b510
Jessica <jesscara.993@gmail.com>
2023-01-24 15:32:42 +0100

    constructor

--- index.php
+++ index.php
@@ -5,4 +5,11 @@ class Movie
   public $title;
   public $year;
   public $duration;
+
+  function __construct($title, $year, $duration)
+  {
+    $this->title = $title;
+    $this->year = $year;
+    $this->duration = $duration;
+  }
 }
