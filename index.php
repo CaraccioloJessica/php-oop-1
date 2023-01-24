5618dfdd709ebf28b77fe5c8b84c79ee8ef86b4b
Jessica <jesscara.993@gmail.com>
2023-01-24 15:56:07 +0100

    method

--- index.php
+++ index.php
@@ -12,10 +12,23 @@ class Movie
     $this->year = $year;
     $this->duration = $duration;
   }
+
+  public function getHtml()
+  {
+    return
+      $this->title
+      . ' - ' . $this->year
+      . ' - ' . $this->duration;
+  }
 }
 
 $movie1 = new Movie("Black Panther", "2018", "134 min");
 $movie2 = new Movie("Iron Man", "2008", "126 min");
-var_dump($movie1);
+// var_dump($movie1);
+// echo "<br><br>";
+// var_dump($movie2);
+
+echo $movie1->getHtml();
 echo "<br><br>";
-var_dump($movie2);
+echo $movie2->getHtml();
+echo "<br><br>";
