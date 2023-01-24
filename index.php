6225f40d3af7504105bd1593b729570b2ad584c5
Jessica <jesscara.993@gmail.com>
2023-01-24 16:13:35 +0100

    added genre

--- index.php
+++ index.php
@@ -3,12 +3,14 @@
 class Movie
 {
   public $title;
+  public $genre;
   public $year;
   public $duration;
 
-  function __construct($title, $year, $duration)
+  function __construct($title, $genre, $year, $duration)
   {
     $this->title = $title;
+    $this->genre = $genre;
     $this->year = $year;
     $this->duration = $duration;
   }
@@ -17,18 +19,19 @@ class Movie
   {
     return
       $this->title
+      . ' - ' . $this->genre
       . ' - ' . $this->year
       . ' - ' . $this->duration;
   }
 }
 
-$movie1 = new Movie("Black Panther", "2018", "134 min");
-$movie2 = new Movie("Iron Man", "2008", "126 min");
+$movie1 = new Movie('Black Panther', 'Azione', '2018', '134 min');
+$movie2 = new Movie('La città incantata', 'Animazione', '2001', '125 min');
 // var_dump($movie1);
 // echo "<br><br>";
 // var_dump($movie2);
 
 echo $movie1->getHtml();
-echo "<br><br>";
+echo '<br><br>';
 echo $movie2->getHtml();
-echo "<br><br>";
+echo '<br><br>';
